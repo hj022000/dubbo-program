6338efc2a1e5efdcdfca1783260dc893980566b3
hj022000 <2500833119@qq.com>
2017-07-04 15:25:24 +0800

    热修复bug

---
+++ dubbo-consumer/src/main/java/com/taomee/bigdata/tj/HotFix.java
@@ -0,0 +1,12 @@
+package com.taomee.bigdata.tj;
+
+/**
+ * Author looper.
+ * Company  TaoMee.Inc, ShangHai.
+ * Date  2017/7/4.
+ */
+public class HotFix {
+    public static void main(String[] args) {
+        System.out.println("热修复bug");
+    }
+}
